da1073814796d50e3fc601997292b114cf47e694
Dane Powell <git@danepowell.com>
2016-08-18 17:27:00 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # binbuddy
-Bin Buddy for Alexa tells you where to put trash
+Bin Buddy for Alexa tells you where to put trash using Whatbin.com. This project is not affiliated with Whatbin or Recology.
+
+## How to use (developers)
+Follow the steps in the following tutorial to create an AWS Lambda function and Alexa Skill (I am not affiliated with this site): http://tobuildsomething.com/2015/08/14/Amazon-Echo-Alexa-Tutorial-The-Definitive-Guide-to-Coding-an-Alexa-Skill/
+
+- Upload assets in the `src` directory to your AWS Lambda function.
+- Copy the speech assets into your Alexa Skill.
+- (optional) use `create_values.js` to update the list of items using values from Whatbin.
+
+## How to use (end users)
+Enable the skill and try asking Alexa:
+
+- “Alexa, ask Bin Buddy where to put envelopes?”
+- “Alexa, ask Bin Buddy where apple cores go?”
+- “Alexa, ask Bin Buddy what bin to put aluminum cans in?”
+
+## Known issues
+Alexa’s custom slot types are heavily biased towards the pre-defined terms. Thus, the “search” functionality on Whatbin will almost never be used, since Alexa will either choose a known term or abort. I tried using AMAZON.LITERAL as the slot type but Alexa then failed most requests.
+
+Bin Buddy currently only returns results for San Francisco. I’d love to extend this to all of the other areas covered by Whatbin (most of the Bay Area and Seattle it seems) but Whatbin uses an annoying stateful architecture that makes it difficult to query other cities.
+
+Pull requests are welcome to address any of these or other issues!
---
+++ src/index.js
@@ -0,0 +1,87 @@
+console.log("Initializing");
+
+var http       = require('http'),
+    AlexaSkill = require('./AlexaSkill'),
+    APP_ID     = process.env.APP_ID;
+
+var url = function(item){
+  item = item.replace(' ', '+');
+  return 'http://whatbin.com/index.php?r=site%2Frequest&search=' + escape(item) + '&yt0=Search';
+};
+
+var getHtmlFromWhatbin = function(item, callback){
+  http.get(url(item), function(res){
+    var body = '';
+
+    res.on('data', function(data){
+      body += data;
+    });
+
+    res.on('end', function(){
+      var result = body;
+      callback(result);
+    });
+
+  }).on('error', function(e){
+    console.log('Error: ' + e);
+  });
+};
+
+var handleItemRequest = function(intent, session, response){
+  var itemName = intent.slots.Item.value;
+  console.log("Handling item request for " + itemName + "...");
+
+  getHtmlFromWhatbin(itemName, function(data){
+    var re = data.match(/<h2>\s*?(.*?)\s*?<\/h2>/g);
+    var message = re[0];
+    message = message.replace(/<h2>/g, '');
+    message = message.replace(/<\/h2>/g, '');
+    message = message.replace(/^\s+|\s+$/g, '');
+    if (message.indexOf("Search results for") !== -1){
+      var regex = /search=(.*?)">[A-Z]/g;
+      var item;
+      message = "I can tell you about the following items: ";
+      while (item = regex.exec(data)) {
+        message = message + item[1] + ",";
+      }
+    }
+    console.log(message);
+    response.tellWithCard(message, "Bin Buddy", message);
+  });
+};
+
+var Item = function(){
+  AlexaSkill.call(this, APP_ID);
+};
+
+Item.prototype = Object.create(AlexaSkill.prototype);
+Item.prototype.constructor = Item;
+
+Item.prototype.eventHandlers.onSessionStarted = function(sessionStartedRequest, session){
+  console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
+};
+
+Item.prototype.eventHandlers.onLaunch = function(launchRequest, session, response){
+  var output = 'Welcome to Bin Buddy. Ask me where to put any item.';
+  var reprompt = 'You can ask me where to put any item. How can I help?';
+
+  response.ask(output, reprompt);
+
+  console.log("onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+};
+
+Item.prototype.intentHandlers = {
+  GetItemIntent: function(intent, session, response){
+    handleItemRequest(intent, session, response);
+  },
+
+  HelpIntent: function(intent, session, response){
+    var speechOutput = 'You can ask where to put items.';
+    response.ask(speechOutput);
+  }
+};
+
+exports.handler = function(event, context) {
+  var skill = new Item();
+  skill.execute(event, context);
+};
